3f64b342ac77a950cb29ce8647689ff1881f04ba
Benjamin K <benjamin.koffler@elca.ch>
2024-06-04 09:58:34 +0200

    add secrets

--- OCR.py
+++ OCR.py
@@ -11,12 +11,6 @@ from utils import class_dict
 import json
 import streamlit as st
 
-#with open("VISION_KEY.json", "r") as json_file:
-    #json_data = json.load(json_file)
-
-#VISION_KEY = json_data["VISION_KEY"]
-#VISION_ENDPOINT = json_data["VISION_ENDPOINT"]
-
 VISION_KEY = st.secrets["VISION_KEY"]
 VISION_ENDPOINT = st.secrets["VISION_ENDPOINT"]
 
